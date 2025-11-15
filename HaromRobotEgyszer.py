207794894daaf05b7dcb557052ea0fd7e16d86a2
Kupi Annamária (SZF_F_2025_2027) <kupi.annamaria@diak.szamalk-szalezi.hu>
2025-11-15 15:50:47 +0100

    projekt szerkezet

---
+++ HaromRobotEgyszer.py
@@ -0,0 +1,31 @@
+import random
+
+def szamgeneralas():
+    szam= random.randint
+    print("Robot tippel")
+    i=0
+    while i<3:
+            robotoktippelnek()
+            ellenorzes()
+    i+= i
+
+
+def robotokletrehozasa():
+    print("Robotok létrehozása")
+    robot1= -1
+    robot2= -1
+    robot3= -1
+
+def robotoktippelnek():
+    print("Robotok tippelnek")
+    robot1 = random.randint (1,10)
+    robot2 = random.randint(1,10)
+    robot3 = random.randint(1,10)
+
+def ellenorzes():
+    if
+    print("Ellenőrzés")
+
+
+
+def nyertes():
